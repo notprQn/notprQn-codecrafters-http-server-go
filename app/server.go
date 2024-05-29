5e0b8d499491b499e79fb74bd50bfb3f3890190c
João Pedro Resende Silva <90474257+notprQn@users.noreply.github.com>
2024-05-29 13:50:39 -0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"bytes"
-
 	"fmt"
 
 	"net"
@@ -10,7 +8,60 @@ import (
 	"os"
 
 	"strings"
+	// Uncomment this block to pass the first stage
+	// "net"
+	// "os"
 )
+
+type Request struct {
+	Method string
+
+	Headers map[string]string
+
+	Path string
+
+	Body string
+}
+
+func ParseRequest(rawReq string) *Request {
+
+	req := &Request{Headers: make(map[string]string)}
+
+	parts := strings.Split(rawReq, "\r\n")
+
+	reqLine := strings.Fields(parts[0])
+
+	req.Method = reqLine[0]
+
+	req.Path = reqLine[1]
+
+	part := 1
+
+	for parts[part] != "" {
+
+		header := strings.Split(parts[part], ": ")
+
+		req.Headers[header[0]] = header[1]
+
+		part++
+
+	}
+
+	part++
+
+	req.Body = parts[part]
+
+	return req
+
+}
+
+type Server struct {
+	Addr string
+
+	Port string
+
+	Fs string
+}
 
 func main() {
 
@@ -20,11 +71,45 @@ func main() {
 
 	// Uncomment this block to pass the first stage
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
+	//
+
+	s := &Server{
+
+		Addr: "0.0.0.0",
+
+		Port: "4221",
 	}
+
+	if len(os.Args) > 2 {
+
+		if os.Args[1] == "--directory" {
+
+			s.Fs = os.Args[2]
+
+			err := os.Chdir(s.Fs)
+
+			if err != nil {
+
+				fmt.Fprintf(os.Stderr, "unable to serve FS %s error: %s", s.Fs, err)
+
+				os.Exit(1)
+
+			}
+
+		}
+
+	}
+
+	l, err := net.Listen("tcp", fmt.Sprintf("%s:%s", s.Addr, s.Port))
+
+	if err != nil {
+
+		fmt.Fprintf(os.Stderr, "Failed to bind to port %s", s.Port)
+
+		os.Exit(1)
+
+	}
+
 	defer l.Close()
 
 	for {
@@ -33,138 +118,108 @@ func main() {
 
 		if err != nil {
 
-			fmt.Println("Error accepting connection: ", err.Error())
+			fmt.Fprintln(os.Stderr, "Error accepting connection: ", err.Error())
 
 			os.Exit(1)
 
 		}
 
-		go handleConnection(conn)
+		go func(conn net.Conn) {
+
+			defer conn.Close()
+
+			buf := make([]byte, 1024)
+
+			n, err := conn.Read(buf)
+
+			if err != nil {
+
+				fmt.Fprintln(os.Stderr, "listening error occurred: ", err)
+
+				os.Exit(1)
+
+			}
+
+			req := ParseRequest(string(buf[:n]))
+
+			if req.Path == "/" {
+
+				conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+
+			}
+
+			if strings.HasPrefix(req.Path, "/echo/") {
+
+				customHeaders := ""
+
+				message := req.Path[len("/echo/"):]
+
+				fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)
+
+				if v, ok := req.Headers["Accept-Encoding"]; ok {
+
+					if v == "gzip" {
+
+						customHeaders += "Content-Encoding: gzip\r\n"
+
+					}
+
+				}
+
+				fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n%s\r\n%s", len(message), customHeaders, message)
+
+			}
+
+			if strings.HasPrefix(req.Path, "/user-agent") {
+
+				message := req.Headers["User-Agent"]
+
+				fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)
+
+			}
+
+			if strings.HasPrefix(req.Path, "/files/") && req.Method == "GET" {
+
+				file := req.Path[len("/files/"):]
+
+				data, err := os.ReadFile(file)
+
+				if err != nil {
+
+					conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+
+					return
+
+				}
+
+				fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(data), data)
+
+			}
+
+			if strings.HasPrefix(req.Path, "/files/") && req.Method == "POST" {
+
+				file := req.Path[len("/files/"):]
+
+				err := os.WriteFile(file, []byte(req.Body), 0666)
+
+				if err != nil {
+
+					conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+
+					return
+
+				}
+
+				fmt.Fprintf(conn, "HTTP/1.1 201 Created\r\n\r\n")
+
+			} else {
+
+				conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+
+			}
+
+		}(conn)
 
 	}
-
-}
-
-func handleConnection(conn net.Conn) {
-
-	//frees memory by closing connection at end of function
-
-	defer conn.Close()
-
-	buf := make([]byte, 1024)
-
-	conn.Read(buf)
-
-	buf = bytes.Trim(buf, "\x00")
-
-	bufString := strings.Split(string(buf), "\n")
-
-	request := strings.Split(bufString[0], " ")
-
-	host := bufString[1]
-
-	// method := request[0]
-
-	method := request[0]
-
-	path := request[1]
-
-	version := request[2]
-
-	fmt.Printf("Port: %s\nPath: %s\nHTTP version: %s\n", host, path, version)
-
-	var response string
-
-	switch {
-
-	case path == "/":
-
-		response = "HTTP/1.1 200 OK\r\n\r\n"
-
-	case strings.Contains(path, "echo"):
-
-		echostring := strings.Split(path, "/")
-
-		response = "HTTP/1.1 200 OK\r\n"
-
-		response += fmt.Sprintf("Content-Type: text/plain\r\nContent-Length: %d\r\n\r\n", len(echostring[2]))
-
-		response += echostring[2]
-
-	case path == "/user-agent":
-
-		user_agent := bufString[2]
-
-		user_agent_echo := strings.Split(user_agent, " ")
-
-		response = "HTTP/1.1 200 OK\r\n"
-
-		// must subtract one becuase length also counts carriage return as character
-
-		response += fmt.Sprintf("Content-Type: text/plain\r\nContent-Length: %d\r\n\r\n", len(user_agent_echo[1])-1)
-
-		response += user_agent_echo[1]
-
-	case method == "GET" && strings.Contains(path, "files"):
-
-		directory := os.Args[2]
-
-		fileName := strings.TrimPrefix(path, "/files/")
-
-		data, err := os.ReadFile(directory + fileName)
-
-		if err != nil {
-
-			response = "HTTP/1.1 404 Not Found\r\n\r\n"
-
-		} else {
-
-			dataString := string(data)
-
-			response = "HTTP/1.1 200 OK\r\n"
-
-			response += fmt.Sprintf("Content-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n", len(dataString))
-
-			response += dataString
-
-		}
-
-	case method == "POST" && strings.Contains(path, "files"):
-
-		directory := os.Args[2]
-
-		fileName := strings.TrimPrefix(path, "/files/")
-
-		filepath := fmt.Sprintf("%s%s", directory, fileName)
-
-		f, err := os.Create(filepath)
-
-		if err != nil {
-
-			fmt.Printf("Unable to create file: %s\n", filepath)
-
-		}
-
-		_, err = f.WriteString(bufString[len(bufString)-1])
-
-		if err != nil {
-
-			fmt.Println("Unable to write to file")
-
-		}
-
-		response = "HTTP/1.1 201 Created\r\n"
-
-		response += fmt.Sprintf("Content-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n", len(bufString[len(bufString)-1]))
-
-		response += bufString[len(bufString)-1]
-
-	default:
-
-		response = "HTTP/1.1 404 Not Found\r\n\r\n"
-
-	}
-
-	conn.Write([]byte(response))
 
 }
