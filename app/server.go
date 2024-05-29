ab8c3d5d2582eacc72ddf1120e69b6190318ebf0
João Pedro Resende Silva <90474257+notprQn@users.noreply.github.com>
2024-05-29 11:03:42 -0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,24 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
-	"os"
+	"net/http"
 )
 
+func Handler(conn net.Conn) {
+	request, err := http.ReadRequest(bufio.NewReader(conn))
+
+	if err != nil {
+		fmt.Println("Error reading request ", err.Error())
+		return
+	}
+
+	if request.URL.Path == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		return
+	}
+	conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+}
+
 func main() {
-	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
 	}
 
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+
+	Handler(conn)
+
 }
